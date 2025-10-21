d19725d84dcb13aeb27d4c58e5020514ba611c68
liraz <lirazpat6@gmail.com>
2025-10-21 04:02:36 +0300

    docker action

--- .github/actions/hello-docker/entarpoint.sh
+++ .github/actions/hello-docker/entarpoint.sh
@@ -3,5 +3,6 @@ echo "::debug::runing entarpoint.sh"
 echo "Hello $1"
 echo "Hello: $HELLO"
 time=$(date)
+echo "man_to_greet=$NAME" >> GITHUB_ENV
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "Hello_TIME=$time" >> GITHUB_ENV
